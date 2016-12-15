d5c6524d3e2363418d25cc7149a0c6d73c6d1ad4
Aaron <aaronjhchong@hotmail.com>
2016-12-15 23:40:40 +0800

    Add demo to Weeks 0 and 2

--- scripts/tooltip.js
+++ scripts/tooltip.js
@@ -1,10 +1,14 @@
 (function() {
     // Maps a string to a tooltip (in the form of a HTML string)
     var tooltips = {
+        'schedule' : 'A plan for carrying out a process or procedure, giving lists of intended events and times.',
+        'i.e.' : 'Short for <i>id est</i>.',
+        'ID'   : '<a href="https://en.wikipedia.org/wiki/ID">identification</a>'
     };
 
     // Maps a string to a list of aliases
     var aliases = {
+        'schedule' : ['schedules', 'scheduling']
     }
 
     for (let term in aliases) {
